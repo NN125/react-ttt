329a4df471a7faab461927e2585d7bf5cfcba50a
NN125 <nicolas.nolden@student.swic.edu>
2025-10-29 02:23:45 -0500

    round out square hover

--- src/App.jsx
+++ src/App.jsx
@@ -106,7 +106,7 @@ export default function App() {
 function Square({ value, onClick }) {
   return (
     <button
-      className="text-9xl font-bold size-36 text-center text-white hover:bg-slate-700 transition-colors duration-200 cursor-pointer"
+      className="text-9xl font-bold size-36 text-center text-white hover:bg-slate-700 rounded-lg transition-colors duration-200 cursor-pointer"
       onClick={onClick}
     >
       {value}
